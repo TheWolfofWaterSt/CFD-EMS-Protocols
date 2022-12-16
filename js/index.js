a3f5edaec86da053a1017af3031373b65cb953fd
James Wolf <59372757+TheWolfofWaterSt@users.noreply.github.com>
2022-12-16 14:31:10 -0600

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function Login() {
+  const enteredPassword = document.getElementById("passwordInput").value;
+
+  if (enteredPassword == "Aspirin") {
+    window.location = "home.html"
+  } else {
+    document.getElementById("errorMsg").innerHTML = "*The password you've entered is incorrect*";
+  }
+}
+
+function enterKeyPressed(event) {
+  if (event.keyCode == 13) {
+    Login();
+  }
+}
+
